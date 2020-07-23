0f5cd58001ab10465beedf3e172bae13d86f9070
ExequielDiaz <exequielandresdiaz@gmail.com>
2020-07-23 12:06:42 -0300

    ahi va

---
+++ Contratos/metadatos/componentes/toba_asistente_abms/dump_111000022.sql
@@ -0,0 +1,121 @@
+------------------------------------------------------------
+--[111000022]--  ass 
+------------------------------------------------------------
+
+------------------------------------------------------------
+-- apex_molde_operacion
+------------------------------------------------------------
+
+--- INICIO Grupo de desarrollo 111
+INSERT INTO apex_molde_operacion (proyecto, molde, operacion_tipo, nombre, item, carpeta_archivos, prefijo_clases, fuente, punto_montaje) VALUES (
+	'Contratos', --proyecto
+	'111000022', --molde
+	'10', --operacion_tipo
+	'ass', --nombre
+	'111000033', --item
+	'ass', --carpeta_archivos
+	'_ass', --prefijo_clases
+	'Contratos', --fuente
+	'111000005'  --punto_montaje
+);
+--- FIN Grupo de desarrollo 111
+
+------------------------------------------------------------
+-- apex_molde_operacion_abms
+------------------------------------------------------------
+INSERT INTO apex_molde_operacion_abms (proyecto, molde, tabla, gen_usa_filtro, gen_separar_pantallas, filtro_comprobar_parametros, cuadro_eof, cuadro_eliminar_filas, cuadro_id, cuadro_forzar_filtro, cuadro_carga_origen, cuadro_carga_sql, cuadro_carga_php_include, cuadro_carga_php_clase, cuadro_carga_php_metodo, datos_tabla_validacion, apdb_pre, punto_montaje) VALUES (
+	'Contratos', --proyecto
+	'111000022', --molde
+	'sexo', --tabla
+	'0', --gen_usa_filtro
+	NULL, --gen_separar_pantallas
+	NULL, --filtro_comprobar_parametros
+	NULL, --cuadro_eof
+	NULL, --cuadro_eliminar_filas
+	'sexo', --cuadro_id
+	NULL, --cuadro_forzar_filtro
+	'datos_tabla', --cuadro_carga_origen
+	'SELECT
+	t_s.sexo,
+	t_s.descsexo
+FROM
+	sexo as t_s
+ORDER BY descsexo', --cuadro_carga_sql
+	NULL, --cuadro_carga_php_include
+	NULL, --cuadro_carga_php_clase
+	NULL, --cuadro_carga_php_metodo
+	NULL, --datos_tabla_validacion
+	NULL, --apdb_pre
+	NULL  --punto_montaje
+);
+
+------------------------------------------------------------
+-- apex_molde_operacion_abms_fila
+------------------------------------------------------------
+
+--- INICIO Grupo de desarrollo 111
+INSERT INTO apex_molde_operacion_abms_fila (proyecto, molde, fila, orden, columna, asistente_tipo_dato, etiqueta, en_cuadro, en_form, en_filtro, filtro_operador, cuadro_estilo, cuadro_formato, dt_tipo_dato, dt_largo, dt_secuencia, dt_pk, elemento_formulario, ef_obligatorio, ef_desactivar_modificacion, ef_procesar_javascript, ef_carga_origen, ef_carga_sql, ef_carga_php_include, ef_carga_php_clase, ef_carga_php_metodo, ef_carga_tabla, ef_carga_col_clave, ef_carga_col_desc, punto_montaje) VALUES (
+	'Contratos', --proyecto
+	'111000022', --molde
+	'111000148', --fila
+	'1', --orden
+	'sexo', --columna
+	'1000001', --asistente_tipo_dato
+	'Sexo', --etiqueta
+	'1', --en_cuadro
+	'1', --en_form
+	'0', --en_filtro
+	'ILIKE', --filtro_operador
+	'4', --cuadro_estilo
+	'1', --cuadro_formato
+	'C', --dt_tipo_dato
+	NULL, --dt_largo
+	'', --dt_secuencia
+	'1', --dt_pk
+	'ef_editable', --elemento_formulario
+	'1', --ef_obligatorio
+	NULL, --ef_desactivar_modificacion
+	NULL, --ef_procesar_javascript
+	NULL, --ef_carga_origen
+	NULL, --ef_carga_sql
+	NULL, --ef_carga_php_include
+	NULL, --ef_carga_php_clase
+	NULL, --ef_carga_php_metodo
+	NULL, --ef_carga_tabla
+	NULL, --ef_carga_col_clave
+	NULL, --ef_carga_col_desc
+	NULL  --punto_montaje
+);
+INSERT INTO apex_molde_operacion_abms_fila (proyecto, molde, fila, orden, columna, asistente_tipo_dato, etiqueta, en_cuadro, en_form, en_filtro, filtro_operador, cuadro_estilo, cuadro_formato, dt_tipo_dato, dt_largo, dt_secuencia, dt_pk, elemento_formulario, ef_obligatorio, ef_desactivar_modificacion, ef_procesar_javascript, ef_carga_origen, ef_carga_sql, ef_carga_php_include, ef_carga_php_clase, ef_carga_php_metodo, ef_carga_tabla, ef_carga_col_clave, ef_carga_col_desc, punto_montaje) VALUES (
+	'Contratos', --proyecto
+	'111000022', --molde
+	'111000149', --fila
+	'2', --orden
+	'descsexo', --columna
+	'1000001', --asistente_tipo_dato
+	'Descsexo', --etiqueta
+	'1', --en_cuadro
+	'1', --en_form
+	'0', --en_filtro
+	'ILIKE', --filtro_operador
+	'4', --cuadro_estilo
+	'1', --cuadro_formato
+	'C', --dt_tipo_dato
+	NULL, --dt_largo
+	'', --dt_secuencia
+	'0', --dt_pk
+	'ef_editable', --elemento_formulario
+	'1', --ef_obligatorio
+	NULL, --ef_desactivar_modificacion
+	NULL, --ef_procesar_javascript
+	NULL, --ef_carga_origen
+	NULL, --ef_carga_sql
+	NULL, --ef_carga_php_include
+	NULL, --ef_carga_php_clase
+	NULL, --ef_carga_php_metodo
+	NULL, --ef_carga_tabla
+	NULL, --ef_carga_col_clave
+	NULL, --ef_carga_col_desc
+	NULL  --punto_montaje
+);
+--- FIN Grupo de desarrollo 111
